f9f328f42d26fa88e7a8bbcd77564958079b212d
cteixeiraVO <cteixeira@ventureoak.com>
2015-10-23 16:24:38 +0100

    added card expiration date input

--- app/src/main/java/com/railway/railway/ActivityRegister.java
+++ app/src/main/java/com/railway/railway/ActivityRegister.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class activity_register extends AppCompatActivity {
+public class ActivityRegister extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
