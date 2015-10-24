eec3cf550acac6824da5ee20d893ac206467b85f
cteixeiraVO <cteixeira@ventureoak.com>
2015-10-24 15:03:25 +0100

    added card expiration date

--- app/src/main/java/com/railway/railway/activity/listeners/RegisterActivityRegisterTask.java
+++ app/src/main/java/com/railway/railway/activity/listeners/RegisterActivityRegisterTask.java
@@ -1,9 +1,11 @@
 package com.railway.railway.activity.listeners;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.widget.Toast;
 
+import com.railway.railway.activity.UserActivity;
 import com.railway.railway.business.api.request.RegisterRequest;
 import com.railway.railway.business.api.request.RegisterRequestData;
 
@@ -48,6 +50,8 @@ public class RegisterActivityRegisterTask extends AsyncTask<Void, Void, JSONObje
             toast = Toast.makeText(this.context,"Nhé... deu asneira",Toast.LENGTH_LONG);
         } else {
             toast = Toast.makeText(this.context,"Parabens Registou-se",Toast.LENGTH_LONG);
+            Intent intent = new Intent(context, UserActivity.class);
+            context.startActivity(intent);
         }
         toast.show();
     }
